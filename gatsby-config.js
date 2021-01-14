87a17f7b3c793801cddb5696e66e5f77a3e450ba
Colin Bull <colin.bull@digital.education.gov.uk>
2021-01-14 12:39:35 +0000

    Correcting ENV VAR

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN,
       },
     },
   ],
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,20 +4,21 @@ import { graphql } from "gatsby"
 import { BLOCKS, MARKS } from "@contentful/rich-text-types"
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
-const Bold = ({ children }) => <span className="govuk-!-font-weight-bold">{children}</span>;
-
-const Text = ({ children }) => <p className="govuk-body">{children}</p>;
 
 const options = {
   renderMark: {
-    [MARKS.BOLD]: (text) => <Bold>{text}</Bold>
+    [MARKS.BOLD]: (text) => <span className="govuk-!-font-weight-bold">{text}</span>,
+    [MARKS.CODE]: (text) => <pre>{text}</pre>
   },
   renderNode: {
     [BLOCKS.HEADING_1]: (node, children) => <h1 className="govuk-heading-xl">{children}</h1>,
     [BLOCKS.HEADING_2]: (node, children) => <h2 className="govuk-heading-l">{children}</h2>,
     [BLOCKS.HEADING_3]: (node, children) => <h3 className="govuk-heading-m">{children}</h3>,
     [BLOCKS.HEADING_4]: (node, children) => <h4 className="govuk-heading-s">{children}</h4>,
-    [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>
+    [BLOCKS.PARAGRAPH]: (node, children) => <p className="govuk-body">{children}</p>,
+    [BLOCKS.UL_LIST]: (node, children) => <ul className="govuk-list govuk-list--bullet">{children}</ul>,
+    [BLOCKS.OL_LIST]: (node, children) => <ul className="govuk-list govuk-list--number">{children}</ul>,
+    [BLOCKS.LIST_ITEM]: (node, children) => <li>{children}</li>,
   }
 };
 
