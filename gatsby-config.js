e9f38ad1c6926f20177a89f0b0809fbdd933d90d
Colin Bull <colin.bull@digital.education.gov.uk>
2021-01-14 10:28:10 +0000

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,29 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,46 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-export default function Home() {
-  return <div>Hello world!</div>
+import { BLOCKS, MARKS } from "@contentful/rich-text-types"
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+
+const Bold = ({ children }) => <span className="govuk-!-font-weight-bold">{children}</span>;
+
+const Text = ({ children }) => <p className="govuk-body">{children}</p>;
+
+const options = {
+  renderMark: {
+    [MARKS.BOLD]: (text) => <Bold>{text}</Bold>
+  },
+  renderNode: {
+    [BLOCKS.HEADING_1]: (node, children) => <h1 className="govuk-heading-xl">{children}</h1>,
+    [BLOCKS.HEADING_2]: (node, children) => <h2 className="govuk-heading-l">{children}</h2>,
+    [BLOCKS.HEADING_3]: (node, children) => <h3 className="govuk-heading-m">{children}</h3>,
+    [BLOCKS.HEADING_4]: (node, children) => <h4 className="govuk-heading-s">{children}</h4>,
+    [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>
+  }
+};
+
+export default function Home({ data }) {
+  return data.allContentfulAdvice.edges.map(({ node },i) => {
+    return <div>
+        <h1 className="govuk-heading-m">{node.title}</h1>
+        <div>{documentToReactComponents(JSON.parse(node.advicebody.raw), options)}</div>
+    </div>
+  })
 }
+
+export const pageQuery = graphql`
+    query {
+        allContentfulAdvice {
+            edges {
+                node {
+                    title,
+                    advicebody {
+                        raw
+                    }
+                }
+            }
+        }
+    }
+`
