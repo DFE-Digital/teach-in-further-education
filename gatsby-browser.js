0bf2d4e92462f86b82b700890cea70ec6d943c16
Colin Bull <colin.bull@digital.education.gov.uk>
2021-01-26 17:23:18 +0000

    Removing layout - temp

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,7 +6,7 @@ import { Layout } from "./src/components/Layout"
 export function wrapPageElement ({ element, props }) {
   // props provide same data to Layout as Page element will get
   // including location, data, etc - you don't need to pass it
-  return <Layout {...props}>{element}</Layout>
+  return element
 }
 
 export function onClientEntry() {
