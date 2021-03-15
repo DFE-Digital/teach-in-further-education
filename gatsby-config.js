6d657bc3fc306a69fe8f7088c44394e6b3094001
Colin Bull <colin.bull@digital.education.gov.uk>
2021-03-15 16:12:07 +0000

    Adding google analytics link

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,5 +53,29 @@ module.exports = {
         downloadLocal: true
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          process.env.GOOGLE_ANALYTICS_TAG, // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: [],
+        },
+      },
+    },
   ],
 }
