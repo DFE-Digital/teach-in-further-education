7e30868c3af01365bbf1bd4bdc809e971b6da646
Colin Bull <colin.bull@digital.education.gov.uk>
2021-04-29 21:26:20 +0100

    Added workingDir to CF CLI action

--- .github/actions/cloudfoundry/entrypoint.sh
+++ .github/actions/cloudfoundry/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -l
 
+if [ -n "$INPUT_WORKINGDIR" ]; then
+  cd "$INPUT_WORKINGDIR"
+fi
+
 cf login -a "$INPUT_CF_API" -u "$INPUT_CF_USERNAME" -p "$INPUT_CF_PASSWORD"
 
 if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
