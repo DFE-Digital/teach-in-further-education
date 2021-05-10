97440419a1f5a263359d9214e9767a8abd6171ad
kelvin <kelvin.atawura@uaccount.uk>
2021-05-10 17:42:53 +0100

    unit test for accordion

---
+++ src/components/__tests__/Accordion.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "@testing-library/react"
+import Accordion from '../Accordion';
+
+describe('Accordion', () => {
+  it('renders correctly', () => {
+    const {getByText} = render(<Accordion title='test title' children='test context' />);
+    
+    expect(getByText("test title")).toBeVisible();
+    expect(getByText("test context")).toBeVisible();
+  });
+});
