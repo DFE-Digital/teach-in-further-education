78f827f7972a2ffefefe34457a34156e16e877a6
kelvin <kelvin.atawura@uaccount.uk>
2021-05-10 20:29:52 +0100

    Keep calm and commit

---
+++ src/components/__tests__/Button.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "@testing-library/react"
+import Button from '../Button';
+
+describe('Button', () => {
+  it('renders correctly', () => {
+    const {getByText} = render(<Button>Click me</Button>);
+    
+    expect(getByText("Click me")).toBeVisible();
+  });
+});
