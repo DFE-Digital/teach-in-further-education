4cc9cfa19f6df778df000f9e0bdaabc48856f7cd
Colin Bull <colin.bull@digital.education.gov.uk>
2021-05-18 08:19:25 +0100

    Explicitly calling target

--- .github/actions/cloudfoundry/entrypoint.sh
+++ .github/actions/cloudfoundry/entrypoint.sh
@@ -6,10 +6,10 @@ fi
 
 cf login -a "$INPUT_CF_API" -u "$INPUT_CF_USERNAME" -p "$INPUT_CF_PASSWORD"
 
-echo "$INPUT_CF_SPACE"
+echo "Space: $INPUT_CF_SPACE"
 
-if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
-  cf target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
-fi
+#if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
+cf target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
+#fi
 
 sh -c "cf7 $*"
