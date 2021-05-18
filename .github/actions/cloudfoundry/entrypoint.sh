74c7d84eedf8066e24951ef518d600a9aace1da8
Colin Bull <colin.bull@digital.education.gov.uk>
2021-05-18 08:56:56 +0100

    changing to call cf7

--- .github/actions/cloudfoundry/entrypoint.sh
+++ .github/actions/cloudfoundry/entrypoint.sh
@@ -4,12 +4,6 @@ if [ -n "$INPUT_WORKINGDIR" ]; then
   cd "$INPUT_WORKINGDIR"
 fi
 
-cf7 login -a "$INPUT_CF_API" -u "$INPUT_CF_USERNAME" -p "$INPUT_CF_PASSWORD"
-
-echo "Space: $INPUT_CF_SPACE"
-
-#if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
-cf7 target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
-#fi
+cf7 login -a "$INPUT_CF_API" -u "$INPUT_CF_USERNAME" -p "$INPUT_CF_PASSWORD" -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
 
 sh -c "cf7 $*"
