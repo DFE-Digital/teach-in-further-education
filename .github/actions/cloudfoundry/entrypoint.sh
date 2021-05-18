4a83c63f0c60336c1c35f224261eb138c6ba4997
Colin Bull <colin.bull@digital.education.gov.uk>
2021-05-18 08:13:42 +0100

    CF_SPACE not being set

--- .github/actions/cloudfoundry/entrypoint.sh
+++ .github/actions/cloudfoundry/entrypoint.sh
@@ -6,6 +6,8 @@ fi
 
 cf login -a "$INPUT_CF_API" -u "$INPUT_CF_USERNAME" -p "$INPUT_CF_PASSWORD"
 
+echo "$INPUT_CF_SPACE"
+
 if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
   cf target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
 fi
