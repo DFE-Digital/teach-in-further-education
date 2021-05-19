03e0f04c7b5b2b8b79546b37a66202c8cf69c2f1
spicy-rice <69350762+spicy-rice@users.noreply.github.com>
2021-05-19 13:43:06 +0100

    testing

--- cypress/e2e/accessibility.test.js
+++ cypress/e2e/accessibility.test.js
@@ -1,10 +1,39 @@
 /// <reference types="Cypress" />
 
-describe("Accessibility tests", () => {
-  beforeEach(() => {
-    cy.visit("/").get("main").injectAxe()
+import { useStaticQuery, graphql } from "gatsby"
+
+const data = useStaticQuery(graphql`
+  query {
+    allContentfulPage {
+      edges {
+        node {
+          slug {
+            title
+          }
+        }
+      }
+    }
+  }
+`)
+
+{
+  data.allContentfulPage.edges.map(edge => {
+    return describe("Accessibility tests", () => {
+      beforeEach(() => {
+        cy.visit(`/${edge.node.slug}`).get("main").injectAxe()
+      })
+      it("Has no detectable accessibility violations on load", () => {
+        cy.checkA11y()
+      })
+    })
   })
-  it("Has no detectable accessibility violations on load", () => {
-    cy.checkA11y()
-  })
-})
+}
+
+// describe("Accessibility tests", () => {
+//   beforeEach(() => {
+//     cy.visit("/").get("main").injectAxe()
+//   })
+//   it("Has no detectable accessibility violations on load", () => {
+//     cy.checkA11y()
+//   })
+// })
