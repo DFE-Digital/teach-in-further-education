2a4191c4f4df79dbbcb6dba65e61ecfad69e0a1d
Colin Bull <colin.bull@digital.education.gov.uk>
2021-05-20 11:41:27 +0100

    Minor update to structure.

--- cypress/e2e/accessibility.test.js
+++ cypress/e2e/accessibility.test.js
@@ -1,32 +1,16 @@
 /// <reference types="Cypress" />
 
-import { useStaticQuery, graphql } from "gatsby"
 
-const data = useStaticQuery(graphql`
-  query {
-    allContentfulPage {
-      edges {
-        node {
-          slug
-        }
-      }
-    }
-  }
-`)
 
-{
-  data.allContentfulPage.edges.map(edge => {
-    return describe("Accessibility tests", () => {
-      cy.task("sitemapLocations").then(pages => {
-        pages.forEach(page => {
+describe("Accessibility tests", () => {
+      cy.task("getPageSlugs").then(slugs => {
+        slugs.forEach(slug => {
           beforeEach(() => {
-            cy.visit(`/${edge.node.slug}`).get("main").injectAxe()
+            cy.visit(`/${slug}`).get("main").injectAxe()
           })
           it("Has no detectable accessibility violations on load", () => {
             cy.checkA11y()
           })
         })
       })
-    })
-  })
-}
+})
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,27 +1,45 @@
 /// <reference types="cypress" />
 
 const fetch = require("node-fetch")
+import { useStaticQuery, graphql } from "gatsby"
 
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
+
+  const data = useStaticQuery(graphql`
+      query {
+          allContentfulPage {
+              edges {
+                  node {
+                      slug
+                  }
+              }
+          }
+      }
+  `)
+
   on("task", {
-    sitemapLocations() {
-      return fetch(`${config.baseUrl}/sitemap.xml`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/xml",
-        },
-      })
-        .then(res => res.text())
-        .then(xml => {
-          const locs = [...xml.matchAll(`<loc>(.|\n)*?</loc>`)].map(([loc]) =>
-            loc.replace("<loc>", "").replace("</loc>", "")
-          )
-          return locs
-        })
-    },
+
+    getPageSlugs() {
+      return data.allContentfulPage.edges.map(edge => edge.node.slug)
+    }
+
+    //   return fetch(`${config.baseUrl}/sitemap.xml`, {
+    //     method: "GET",
+    //     headers: {
+    //       "Content-Type": "application/xml",
+    //     },
+    //   })
+    //     .then(res => res.text())
+    //     .then(xml => {
+    //       const locs = [...xml.matchAll(`<loc>(.|\n)*?</loc>`)].map(([loc]) =>
+    //         loc.replace("<loc>", "").replace("</loc>", "")
+    //       )
+    //       return locs
+    //     })
+    // },
   })
   return config
 }
