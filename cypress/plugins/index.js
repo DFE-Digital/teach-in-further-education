400d8b879c353fc27b85bd6cd4833b3a05314c72
spicy-rice <69350762+spicy-rice@users.noreply.github.com>
2021-05-23 14:44:10 +0100

    fetch

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,30 +1,32 @@
 /// <reference types="cypress" />
 
 const fetch = require("node-fetch")
-import { useStaticQuery, graphql } from "gatsby"
+// import { useStaticQuery, graphql } from "gatsby"
+
+const { useStaticQuery } = require("gatsby")
+const { graphql } = require("gatsby")
 
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
-
   const data = useStaticQuery(graphql`
-      query {
-          allContentfulPage {
-              edges {
-                  node {
-                      slug
-                  }
-              }
+    query {
+      allContentfulPage {
+        edges {
+          node {
+            slug
           }
+        }
       }
+    }
   `)
 
   on("task", {
-
     getPageSlugs() {
       return data.allContentfulPage.edges.map(edge => edge.node.slug)
-    }
+    },
+
 
     //   return fetch(`${config.baseUrl}/sitemap.xml`, {
     //     method: "GET",
@@ -43,3 +45,24 @@ module.exports = (on, config) => {
   })
   return config
 }
+
+
+await fetch ('https://graphql.contentful.com/content/v1/spaces/CONTENTFUL_SPACE_ID/environments/CONTENTFUL_DELIVERY_TOKEN?query=query{allContentfulPage{edges{node{slug}}}}&variables={"preview":true}', {
+  method: 'GET', 
+  headers: {"Content-Type": "application/json"}, 
+  body: JSON.stringify(
+    { query:
+      query {
+      allContentfulPage {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+      }
+      })
+      return Response.json()
+      .then(res => res.json())
+    })
+   
