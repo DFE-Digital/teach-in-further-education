b0366ca066682e2920395641e626285c44515b78
Colin Bull <colin.bull@digital.education.gov.uk>
2021-05-24 11:01:42 +0100

    Adding conditional renderering on advice

--- src/components/Advice.js
+++ src/components/Advice.js
@@ -1,10 +1,10 @@
 import React from "react"
 
 export const Advice = ({ id, data, noTitle, ...props }) => {
-    return <div id={id} {...props}>
+    return data.adviceBody !== null ?  (<div id={id} {...props}>
       { noTitle ? null : <h2 className="govuk-heading-l">{data.title}</h2> }
       <div dangerouslySetInnerHTML={{__html:data.adviceBody.childMarkdownRemark.html}} />
-    </div>
+    </div>) : null
 }
 
 export default Advice
