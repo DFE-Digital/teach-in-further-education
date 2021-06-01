effa2a96d9e13a22800af59cc299478fb4076f92
Colin Bull <colin.bull@digital.education.gov.uk>
2021-06-01 22:38:39 +0100

    Tweaking build working directory

--- .github/actions/cloudfoundry/entrypoint.sh
+++ .github/actions/cloudfoundry/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-cd "$INPUT_WORKINGDIR"
+cd "$INPUT_WORKING_DIR"
 cf7 login -a "$INPUT_CF_API" -u "$INPUT_CF_USERNAME" -p "$INPUT_CF_PASSWORD" -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
 
 sh -c "cf7 $*"
