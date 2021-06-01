eda3d1f4d13ffdc85b51a06950c3e5cc5239ac39
Colin Bull <colin.bull@digital.education.gov.uk>
2021-06-01 22:48:29 +0100

    Adding contentful extration

--- .github/actions/cloudfoundry/entrypoint.sh
+++ .github/actions/cloudfoundry/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -l
 
-cd "$INPUT_WORKING_DIR"
+if [ -n "$INPUT_WORKING_DIR" ]; then
+  cd "$INPUT_WORKING_DIR"
+fi
+
 cf7 login -a "$INPUT_CF_API" -u "$INPUT_CF_USERNAME" -p "$INPUT_CF_PASSWORD" -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
-cf7 $*
+
+sh -c "cf7 $*"
