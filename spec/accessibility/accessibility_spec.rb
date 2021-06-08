7fee127522ec2a009e55092c8a9fc8eefd188854
Colin Bull <colin.bull@digital.education.gov.uk>
2021-06-09 00:26:20 +0100

    Making accessibility tests run across all pages

--- spec/accessibility/accessibility_spec.rb
+++ spec/accessibility/accessibility_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe 'Pages should be accessible', :type => :feature do
-  before do
-    visit '/'
-  end
-
-  it 'is axe clean' do
-    expect(page).to be_axe_clean
+  Dir.glob("data/site/pages/**/*.yaml").each do |file|
+    p = YAML.load(File.read(file))
+    it "Ensure #{p[:slug]} is axe compliant" do
+      visit "/#{p[:slug]}"
+      expect(page).to be_axe_clean
+    end
   end
 end
