c0dce691c5eb1f15150df66333944abcb5ddc971
spicy-rice <69350762+spicy-rice@users.noreply.github.com>
2021-07-02 12:46:51 +0100

    header testing

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -16,8 +16,18 @@ describe 'Navigation', :type => :feature do
     link = page.find_by_id('15FI0WUAPnHGy98eB7XW5N')
     within link do
       within 'a' do
+        has_link? 'ways-to-train'
         has_content? 'Ways to train'
       end
     end
   end
+
+
+  it 'renders the drop down' do
+    link = page.find_by_id('41US5qsuhy3YToT8Rjbznc')
+    within link do
+    has_selector 'a#30zC6ehmTs7QXh0ymjNUpI'
+  end
+end
+end
 end
