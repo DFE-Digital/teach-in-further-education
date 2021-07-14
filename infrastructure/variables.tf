6fe9556d026001dd2305a7c665c43d1a128058da
Stuart Kimberley Dunning <stuart.kd@hippodigital.co.uk>
2021-07-14 08:20:17 +0100

    adding terraform again

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -4,6 +4,10 @@ variable "paas_exporter_password" {}
 
 variable "grafana_admin_password" {}
 
+output "paas_username" {
+  value = var.paas_exporter_username
+}
+
 locals {
   paas_api_url               = "https://api.london.cloud.service.gov.uk"
 }
