f1ec9ac19adfe1a8b861cd717c127d014c3c928f
Colin Bull <colin.bull@digital.education.gov.uk>
2021-07-16 11:49:34 +0100

    Fixing merge conflict

--- spec/features/advice_spec.rb
+++ spec/features/advice_spec.rb
@@ -5,18 +5,8 @@ describe 'Advice', :type => :feature do
     visit '/'
   end
 
-  it "can render an with background" do
+  it "has correct class" do
     advice = page.find_by_id("11nV0pkqrJOxP6o2F7KVGx")
-    expect(advice[:class]).to eq "app-advice shaded"
-  end
-
-  it "can render as a callout on left" do
-    advice = page.find_by_id("5dr70Vcxk5sQLytyEibhDN")
-    expect(advice).to have_css "div .call-out-border-left"
-  end
-
-  it "can render as a callout on right" do
-    advice = page.find_by_id("3i4erSWLCL6KDJMuy72dEF")
-    expect(advice).to have_css "div .call-out-border-right"
+    expect(advice[:class]).to eq "app-advice"
   end
 end
