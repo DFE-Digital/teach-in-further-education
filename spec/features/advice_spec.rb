17d71e7717ef1e1627e4474111679f8146a84fa1
Colin <colinbull@users.noreply.github.com>
2021-07-16 14:26:25 +0100

    Development (#15)
    * Minor more header fixes
    
    * Fixing merge conflict
    
    * Fixing merge conflict
    
    * Adding home link.
    
    Co-authored-by: Colin Bull <colin.bull@digital.education.gov.uk>

--- spec/features/advice_spec.rb
+++ spec/features/advice_spec.rb
@@ -5,7 +5,7 @@ describe 'Advice', :type => :feature do
     visit '/'
   end
 
-  it "can render an with background" do
+  it "has correct class" do
     advice = page.find_by_id("11nV0pkqrJOxP6o2F7KVGx")
     expect(advice[:class]).to eq "app-advice shaded"
   end
