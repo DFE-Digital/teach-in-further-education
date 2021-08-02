e98d5cefd7bda4e6fad2e332ff851bee7b8110f3
JY <jean-yves.betton@digital.education.gov.uk>
2021-08-02 11:39:26 +0100

    expanded first set of tests

--- spec/features/question_component_spec.rb
+++ spec/features/question_component_spec.rb
@@ -3,11 +3,27 @@ require 'spec_helper'
 describe 'Find a suitable qualification', :type => :feature do
     it "should have correct radio buttons" do
       visit '/experience-and-qualifications.html'
-      none = page.find_by_id("3cChhb7mHTk4CApUy4Grbf")
-      within none do
-      expect(none[:text]).to eq "None"
-      end
 
-      
+      radio button = page.find_by_id("qNRtpmv245PlK1GAu8Mzm")
+      within radio button do 
+        has_css? 'div', count: 4
+
+      label1 = page.find_by_id("3cChhb7mHTk4CApUy4Grbf")
+      within label1 do
+      expect(label1[:class]).to eq "govuk-radios__item"
+
+      label2 = page.find_by_id("k2NflABnJCxRk260SUr7l")
+      within label2 do
+      expect(label2[:class]).to eq "govuk-radios__item"
+
+      label3 = page.find_by_id("3nEGsh2BzZPp9zYGq6D7FA")
+      within label3 do
+      expect(label3[:class]).to eq "govuk-radios__item"
+
+      label4 = page.find_by_id("2kLaG1uO0pXkHSFosd6tdx")
+      within label4 do
+      expect(label4[:class]).to eq "govuk-radios__item"
+
+      end
     end
   end
