58ee7e53ef3cccf92bcd769d9a2b542d64194b50
Colin Bull <colin.bull@digital.education.gov.uk>
2021-08-02 23:02:13 +0100

    Removeing test logging for analytics

--- js/analytics.js
+++ js/analytics.js
@@ -2,13 +2,6 @@
 export class Analytics {
 
     sendAnalytics(category, action, label) {
-        console.log('analytics', {
-            hitType: 'event',
-            eventCategory: category,
-            eventAction: action,
-            eventLabel: label
-        });
-
         gtag('event', action, {
             'event_category': category,
             'event_label': label
