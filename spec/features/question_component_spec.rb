22dd3d58556f36a6cfd38595825ff2439a4b24dd
JY <jean-yves.betton@digital.education.gov.uk>
2021-08-03 16:54:49 +0100

    Tidying

--- spec/features/question_component_spec.rb
+++ spec/features/question_component_spec.rb
@@ -4,52 +4,46 @@ describe 'Question component', :type => :feature do
 
     it "should have correct radio buttons" do
       visit '/experience-and-qualifications.html'
-
       questionForm = page.find_by_id("qNRtpmv245PlK1GAu8Mzm")
       within questionForm do 
         has_css? 'div', count: 4
-        end
+      end
     end
 
     it "should have text against label1" do
-        visit '/experience-and-qualifications.html'
-
+      visit '/experience-and-qualifications.html'
       label1 = page.find_by_id("3cChhb7mHTk4CApUy4Grbf")
       within label1 do
       expect(label1[:class]).to eq "govuk-label govuk-radios__label"
-        end
+      end
     end
 
     it "should have text against label2" do
-        visit '/experience-and-qualifications.html'
-
+      visit '/experience-and-qualifications.html'
       label2 = page.find_by_id("k2NflABnJCxRk260SUr7l")
       within label2 do
       expect(label2[:class]).to eq "govuk-label govuk-radios__label"
-        end
+      end
     end
 
     it "should have text against label3" do
-        visit '/experience-and-qualifications.html'
-
+      visit '/experience-and-qualifications.html'
       label3 = page.find_by_id("3nEGsh2BzZPp9zYGq6D7FA")
       within label3 do
       expect(label3[:class]).to eq "govuk-label govuk-radios__label"
-        end
+      end
     end
 
     it "should have text against label4" do
-        visit '/experience-and-qualifications.html'
-
+      visit '/experience-and-qualifications.html'
       label4 = page.find_by_id("2kLaG1uO0pXkHSFosd6tdx")
       within label4 do
       expect(label4[:class]).to eq "govuk-label govuk-radios__label"
-        end
+      end
     end
 
       it "should let user select a label and submit" do
         visit '/experience-and-qualifications.html'
-
         radioButton = page.find_by_id("qNRtpmv245PlK1GAu8Mzm")
         within radioButton do 
           submit
