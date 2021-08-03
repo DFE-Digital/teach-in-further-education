4572c78e53d6300bb3bffa272ff490aab778fe88
JY <jean-yves.betton@digital.education.gov.uk>
2021-08-03 23:46:50 +0100

    refactoring

--- spec/features/question_component_spec.rb
+++ spec/features/question_component_spec.rb
@@ -5,76 +5,25 @@ describe 'Question component', :type => :feature do
       visit '/find-the-right-teaching-qualification-for-you.html'
     end
 
-    it "should have correct radio buttons" do
+    it "should have correct elements" do
       questionForm = page.find_by_id("qNRtpmv245PlK1GAu8Mzm")
       within questionForm do 
         has_css? 'div', count: 4
       end
     end
 
-    before do
-      visit '/find-the-right-teaching-qualification-for-you.html'
-    end
-
-    it "should have text against label1" do
-      visit '/find-the-right-teaching-qualification-for-you.html'
-      label1 = page.find_by_id("3cChhb7mHTk4CApUy4Grbf")
-      within label1 do
-      expect(label1[:class]).to eq "govuk-label govuk-radios__label"
+    it "should have correct class" do
+      questionForm = page.find_by_id("qNRtpmv245PlK1GAu8Mzm")
+      within questionForm do
+        expect(questionForm[:class]).to eq "app-question"
       end
     end
 
-    before do
-      visit '/find-the-right-teaching-qualification-for-you.html'
-    end
-
-    it "should have text against label2" do
-      visit '/find-the-right-teaching-qualification-for-you.html'
-      label2 = page.find_by_id("k2NflABnJCxRk260SUr7l")
-      within label2 do
-      expect(label2[:class]).to eq "govuk-label govuk-radios__label"
-      end
-    end
-
-    before do
-      visit '/find-the-right-teaching-qualification-for-you.html'
-    end
-
-    it "should have text against label3" do
-      visit '/find-the-right-teaching-qualification-for-you.html'
-      label3 = page.find_by_id("3nEGsh2BzZPp9zYGq6D7FA")
-      within label3 do
-      expect(label3[:class]).to eq "govuk-label govuk-radios__label"
-      end
-    end
-
-    before do
-      visit '/find-the-right-teaching-qualification-for-you.html'
-    end
-
-    it "should have text against label4" do
-      visit '/find-the-right-teaching-qualification-for-you.html'
-      label4 = page.find_by_id("2kLaG1uO0pXkHSFosd6tdx")
-      within label4 do
-      expect(label4[:class]).to eq "govuk-label govuk-radios__label"
-      end
-    end
-
-    before do
-      visit '/find-the-right-teaching-qualification-for-you.html'
-    end
-
-    it "should let user select a label and submit" do
-      visit '/find-the-right-teaching-qualification-for-you.html'
-      radioButton = page.find_by_id("qNRtpmv245PlK1GAu8Mzm")
-      within radioButton do 
-        submit
-
+    it "should have a disabled submit button" do
       submitButton = page.find_by_id("question-submit-qNRtpmv245PlK1GAu8Mzm")
-      within submitButton do
-        submitQuestionForm
+      within submitButton do 
+        expect(submitButton[:class]).to eq "govuk-button"
       end
     end
   end
-end
   
