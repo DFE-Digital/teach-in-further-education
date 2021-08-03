509ca6d52033f3a4c4cf657956fe688c983aaab8
JY <jean-yves.betton@digital.education.gov.uk>
2021-08-03 20:56:41 +0100

    Fix draft to Ids not being found

--- spec/features/question_component_spec.rb
+++ spec/features/question_component_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 
 describe 'Question component', :type => :feature do
+    before do
+      visit '/find-the-right-teaching-qualification-for-you.html'
+    end
 
     it "should have correct radio buttons" do
-      visit '/experience-and-qualifications.html'
       questionForm = page.find_by_id("qNRtpmv245PlK1GAu8Mzm")
       within questionForm do 
         has_css? 'div', count: 4
@@ -11,7 +13,7 @@ describe 'Question component', :type => :feature do
     end
 
     it "should have text against label1" do
-      visit '/experience-and-qualifications.html'
+      visit '/find-the-right-teaching-qualification-for-you.html'
       label1 = page.find_by_id("3cChhb7mHTk4CApUy4Grbf")
       within label1 do
       expect(label1[:class]).to eq "govuk-label govuk-radios__label"
@@ -19,7 +21,7 @@ describe 'Question component', :type => :feature do
     end
 
     it "should have text against label2" do
-      visit '/experience-and-qualifications.html'
+      visit '/find-the-right-teaching-qualification-for-you.html'
       label2 = page.find_by_id("k2NflABnJCxRk260SUr7l")
       within label2 do
       expect(label2[:class]).to eq "govuk-label govuk-radios__label"
@@ -27,7 +29,7 @@ describe 'Question component', :type => :feature do
     end
 
     it "should have text against label3" do
-      visit '/experience-and-qualifications.html'
+      visit '/find-the-right-teaching-qualification-for-you.html'
       label3 = page.find_by_id("3nEGsh2BzZPp9zYGq6D7FA")
       within label3 do
       expect(label3[:class]).to eq "govuk-label govuk-radios__label"
@@ -35,7 +37,7 @@ describe 'Question component', :type => :feature do
     end
 
     it "should have text against label4" do
-      visit '/experience-and-qualifications.html'
+      visit '/find-the-right-teaching-qualification-for-you.html'
       label4 = page.find_by_id("2kLaG1uO0pXkHSFosd6tdx")
       within label4 do
       expect(label4[:class]).to eq "govuk-label govuk-radios__label"
@@ -43,7 +45,7 @@ describe 'Question component', :type => :feature do
     end
 
       it "should let user select a label and submit" do
-        visit '/experience-and-qualifications.html'
+        visit '/find-the-right-teaching-qualification-for-you.html'
         radioButton = page.find_by_id("qNRtpmv245PlK1GAu8Mzm")
         within radioButton do 
           submit
