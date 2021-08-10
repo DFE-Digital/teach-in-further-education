645793dfbf7bffe70e8281cd8c5b87cf6a05352a
Colin Bull <colin.bull@digital.education.gov.uk>
2021-08-10 21:12:35 +0100

    Fixing up header issues.

---
+++ js/header.js
@@ -0,0 +1,19 @@
+export class Header {
+
+    init(headerId) {
+        const headerElement = document.getElementById(headerId)
+
+        for (const listElement of headerElement.getElementsByTagName("li")) {
+            for (const anchor of listElement.getElementsByTagName("a")){
+                const route = anchor.getAttribute("href")
+                const link = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
+                if(link === route) {
+                    anchor.classList.add("active")
+                } else {
+                    anchor.classList.remove("active")
+                }
+            }
+        }
+    }
+
+}
