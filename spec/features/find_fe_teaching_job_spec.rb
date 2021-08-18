4309ffdb47f9fd285cdb2834cebc42c394e16045
JY <jean-yves.betton@digital.education.gov.uk>
2021-08-18 14:02:23 +0100

    updating element ids in tests and assertions

--- spec/features/find_fe_teaching_job_spec.rb
+++ spec/features/find_fe_teaching_job_spec.rb
@@ -6,33 +6,31 @@ describe 'Find an fe teaching job components', :type => :feature do
     end
 
     it "should have correct elements" do
-      skillsTradesElement = page.find_by_id("2dSAy6Mk1OlBMIiVDcPJ0u")
-      within skillsTradesElement do 
-        has_css? 'govuk-heading-l', count: 1
-        has_css? 'govuk-heading-m', count: 1
-        has_css? 'govuk-body', count: 7
-      end
-      academicSubjectsElement = page.find_by_id("e0XCvhr4XVEtz0N0hZMCf")
-      within academicSubjectsElement do 
-        has_css? 'govuk-heading-l', count: 1
-        has_css? 'govuk-heading-s', count: 1
-        has_css? 'govuk-body', count: 2
-      end
-      chooseYourQualificationElement = page.find_by_id("1xTlxAkJL5DzPuOCDYOsQG")
-      within chooseYourQualificationElement do 
+      feProvidersElement = page.find_by_id("3J3oEV3PF62bJRcXhpLDOH")
+      within feProvidersElement do 
         has_css? 'govuk-heading-l', count: 1
         has_css? 'govuk-list govuk-list--bullet', count: 1
-        has_css? 'govuk-body', count: 6
-        has_css? 'li', count: 4
+        has_css? 'govuk-body', count: 3
+        has_css? 'li', count: 2
+      end
+      learnWhileYouLearnElement = page.find_by_id("1TvwovJDqZuFg4Eqwx4Bu2")
+      within learnWhileYouLearnElement do 
+        has_css? 'govuk-heading-l', count: 1
+        has_css? 'govuk-body', count: 2
+      end
+      carreerChangersElement = page.find_by_id("5sdBDktTAPtjx0YlzHUdGD")
+      within carreerChangersElement do 
+        has_css? 'govuk-heading-l', count: 1
+        has_css? 'govuk-body', count: 2
       end
     end
 
     it "should have correct classes" do
-      skillsTradesElement = page.find_by_id("2dSAy6Mk1OlBMIiVDcPJ0u")
+      skillsTradesElement = page.find_by_id("3J3oEV3PF62bJRcXhpLDOH")
       within skillsTradesElement do
         expect(skillsTradesElement[:class]).to eq "app-advice "
       end
-      academicSubjectsElement = page.find_by_id("e0XCvhr4XVEtz0N0hZMCf")
+      academicSubjectsElement = page.find_by_id("1TvwovJDqZuFg4Eqwx4Bu2")
       within academicSubjectsElement do
         expect(academicSubjectsElement[:class]).to eq "app-advice "
       end
@@ -47,9 +45,9 @@ describe 'Find an fe teaching job components', :type => :feature do
     end
 
     it "links should re-direct to expected urls" do
-      link = page.find_link("What levels mean", match: :first)
+      link = page.find_link("Find your local FE college on the Association of Colleges website", match: :first)
       within link do 
-        expect(link[:href]).to eq 'https://www.gov.uk/what-different-qualification-levels-mean/list-of-qualification-levels'
+        expect(link[:href]).to eq 'https://www.aocjobs.com/employers-a-z'
       end
       link = page.find_link("There are bursaries available for some subjects.")
       within link do 
