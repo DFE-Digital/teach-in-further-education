81d4fae05a3d1776a6a8d6259215c2106d0b77af
JY <jean-yves.betton@digital.education.gov.uk>
2021-08-19 10:52:13 +0100

    More tests and assertions

--- spec/features/support_to_train_spec.rb
+++ spec/features/support_to_train_spec.rb
@@ -16,12 +16,13 @@ describe ' components', :type => :feature do
         has_css? 'govuk-heading-l', count: 1
         has_css? 'govuk-body', count: 3
       end
-      chooseYourQualificationElement = page.find_by_id("1xTlxAkJL5DzPuOCDYOsQG")
-      within chooseYourQualificationElement do 
-        has_css? 'govuk-heading-l', count: 1
-        has_css? 'govuk-list govuk-list--bullet', count: 1
+      availableFundingElement = page.find_by_id("5hTTiOCjK33slAKAiPFby")
+      within availableFundingElement do 
+        has_css? 'govuk-heading-m', count: 1
+        has_css? 'govuk-heading-s', count: 1
+        has_css? 'govuk-list govuk-list--bullet', count: 2
         has_css? 'govuk-body', count: 6
-        has_css? 'li', count: 4
+        has_css? 'li', count: 6
       end
     end
 
@@ -36,24 +37,24 @@ describe ' components', :type => :feature do
         expect(trainBeforeYouApplyElement[:class]).to eq "app-advice "
         have_selector 'http://images.ctfassets.net/n4docnlbw89d/xrbpAnsDT493GYrkv5yL5/8cd895925fecfbc818fe6cb5ae6edf87/TrainingBeforeWork.png?w=475'
       end
-      chooseYourQualificationElement = page.find_by_id("1xTlxAkJL5DzPuOCDYOsQG")
-      within chooseYourQualificationElement do
-        expect(chooseYourQualificationElement[:class]).to eq "app-advice "
-      end
-      listofCoursesElement = page.find_by_id("2C22TEQvPFHAAef6z96a1x")
-      within listofCoursesElement do
-        expect(listofCoursesElement[:class]).to eq "app-advice "
+      availableFundingElement = page.find_by_id("5hTTiOCjK33slAKAiPFby")
+      within availableFundingElement do
+        expect(availableFundingElement[:class]).to eq "app-advice "
       end
     end
-
+t
     it "links should re-direct to expected urls" do
-      link = page.find_link("What levels mean", match: :first)
+      link = page.find_link("You may be eligible to apply for a student loan to help pay for the course fees", match: :first)
       within link do 
-        expect(link[:href]).to eq 'https://www.gov.uk/what-different-qualification-levels-mean/list-of-qualification-levels'
+        expect(link[:href]).to eq 'https://www.gov.uk/student-finance'
       end
-      link = page.find_link("There are bursaries available for some subjects.")
+      link = page.find_link("Find a job in FE on the Association of Colleges website")
       within link do 
-        expect(link[:href]).to eq 'https://www.gov.uk/government/publications/initial-teacher-training-itt-bursary-funding-manual/initial-teacher-training-bursaries-funding-manual-2020-to-2021-academic-year'
+        expect(link[:href]).to eq 'https://www.aocjobs.com/'
+      end
+      link = page.find_link("Find out about the teacher training bursaries available")
+      within link do 
+        expect(link[:href]).to eq 'https://www.gov.uk/government/publications/fe-funding-initial-teacher-education-ite-academic-year-2020-to-2021'
       end
     end
 end
