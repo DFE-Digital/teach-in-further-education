85bc233fe25484dfa270e024fd927b8b14fbfb3c
JY <jean-yves.betton@digital.education.gov.uk>
2021-08-20 04:29:01 +0100

    Updated elements, assertions

--- spec/features/help_and_advice_spec.rb
+++ spec/features/help_and_advice_spec.rb
@@ -13,50 +13,38 @@ describe 'Help and advice components', :type => :feature do
         has_css? 'govuk-list govuk-list--bullet', count: 1
         has_css? 'li', count: 4
       end
-      trainBeforeYouApplyElement = page.find_by_id("12qLcJTqyRLLx3q5FjUBVA")
-      within trainBeforeYouApplyElement do 
+      aboutOurAdvisorsElement = page.find_by_id("6UkTGPOiLhwqP34pJGveU3")
+      within aboutOurAdvisorsElement do 
         has_css? 'govuk-heading-l', count: 1
-        has_css? 'govuk-body', count: 3
+        has_css? 'govuk-body', count: 1
       end
-      availableFundingElement = page.find_by_id("5hTTiOCjK33slAKAiPFby")
-      within availableFundingElement do 
-        has_css? 'govuk-heading-m', count: 1
-        has_css? 'govuk-heading-s', count: 1
-        has_css? 'govuk-list govuk-list--bullet', count: 2
-        has_css? 'govuk-body', count: 6
-        has_css? 'li', count: 6
+      contactUsElement = page.find_by_id("44g2WLYJyWiyj65vmglyZy")
+      within contactUsElement do 
+        has_css? 'govuk-heading-l', count: 1
+        has_css? 'govuk-body', count: 2
+        have_selector 'http://images.ctfassets.net/n4docnlbw89d/3E26qZv6tkg9xfz6YKELoY/551868e774800f6399b71a944466c664/Helpandadvice.png?w=475'
       end
     end
 
     it "should have correct classes" do
-      trainingOnTheJobElement = page.find_by_id("5cu3CALGtvPugA7rbdsPwW")
-      within trainingOnTheJobElement do
-        expect(trainingOnTheJobElement[:class]).to eq "app-advice "
-        have_selector 'http://images.ctfassets.net/n4docnlbw89d/lza5filA0VlqXNUB0X7HJ/6132507391a068275e4412dc6c05fd98/EarnWhileYouLearn.png?w=475'
+      howWeCanHelpElement = page.find_by_id("4CvK0Ica4yZWT5FYtTZdWZ")
+      within howWeCanHelpElement do
+        expect(howWeCanHelpElement[:class]).to eq "app-advice "
       end
-      trainBeforeYouApplyElement = page.find_by_id("12qLcJTqyRLLx3q5FjUBVA")
-      within trainBeforeYouApplyElement do
-        expect(trainBeforeYouApplyElement[:class]).to eq "app-advice "
-        have_selector 'http://images.ctfassets.net/n4docnlbw89d/xrbpAnsDT493GYrkv5yL5/8cd895925fecfbc818fe6cb5ae6edf87/TrainingBeforeWork.png?w=475'
+      aboutOurAdvisorsElement = page.find_by_id("6UkTGPOiLhwqP34pJGveU3")
+      within aboutOurAdvisorsElement do
+        expect(aboutOurAdvisorsElement[:class]).to eq "app-advice "
       end
-      availableFundingElement = page.find_by_id("5hTTiOCjK33slAKAiPFby")
-      within availableFundingElement do
-        expect(availableFundingElement[:class]).to eq "app-advice "
+      contactUsElement = page.find_by_id("44g2WLYJyWiyj65vmglyZy")
+      within contactUsElement do
+        expect(contactUsElement[:class]).to eq "app-advice "
       end
     end
 
     it "links should re-direct to expected urls" do
-      link = page.find_link("You may be eligible to apply for a student loan to help pay for the course fees", match: :first)
+      link = page.find_link("Teach.FE@education.gov.uk")
       within link do 
-        expect(link[:href]).to eq 'https://www.gov.uk/student-finance'
-      end
-      link = page.find_link("Find a job in FE on the Association of Colleges website")
-      within link do 
-        expect(link[:href]).to eq 'https://www.aocjobs.com/'
-      end
-      link = page.find_link("Find out about the teacher training bursaries available")
-      within link do 
-        expect(link[:href]).to eq 'https://www.gov.uk/government/publications/fe-funding-initial-teacher-education-ite-academic-year-2020-to-2021'
+        expect(link[:href]).to eq 'mailto:Teach.FE@education.gov.uk'
       end
     end
   end
