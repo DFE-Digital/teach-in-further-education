dfa18791bddf45fa550ce63498478e55cde0a4cf
Colin Bull <colin.bull@digital.education.gov.uk>
2021-09-01 00:04:50 +0100

    Setting title = navTitle and rearranging in editor

--- contentful/migrations/3.js
+++ contentful/migrations/3.js
@@ -1,13 +1,4 @@
 module.exports = function (migration) {
     const page = migration.editContentType('page');
     page.moveField('navTitle').afterField('title');
-
-    migration.transformEntries({
-        contentType: 'page',
-        from: ['title'],
-        to: ['navTitle'],
-        transformEntryForLocale: function (fromFields, currentLocale) {
-            return { navTitle: fromFields.title[currentLocale] };
-        }
-    });
 };
