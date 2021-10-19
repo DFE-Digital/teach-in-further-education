e077b1665b7822304f3c03ba59e3f481c12f76ce
jY Bee <jybetton7@outlook.com>
2021-10-19 20:07:13 +0100

    More class assertions

--- spec/features/accordion_spec.rb
+++ spec/features/accordion_spec.rb
@@ -5,17 +5,30 @@ describe 'Accordion components', :type => :feature do
     visit '/accordion.html'
   end
 
-  it "should have correct number of steps" do
+  it "should have correct elements" do
     steps = page.find_by_id("26o7zVgA8zuq4GnxJgvjD8")
     within steps do
       has_css? 'div', count: 3
+      has_css? 'govuk-accordion', count: 1
     end
   end
 
-  it "should have the right classes" do
-    defaultContentElement = page.find_by_id("accordion-default-content-0")
+  it "should have correct classes" do
+    defaultContentElement = page.find_by_id("26o7zVgA8zuq4GnxJgvjD8")
     within defaultContentElement do
-      expect(defaultContentElement[:class]).to eq "govuk-accordion__section-content"
+      expect(defaultContentElement[:class]).to eq "govuk-accordion"
+    end
+    accordionDefaultButton = page.find_by_id("accordion-default-heading-0")
+    within accordionDefaultButton do
+      expect(accordionDefaultButton[:class]).to eq "govuk-accordion__section-button" 
+    end
+    accordionDefaultButton1 = page.find_by_id("accordion-default-heading-1")
+    within accordionDefaultButton1 do
+      expect(accordionDefaultButton1[:class]).to eq "govuk-accordion__section-button"
+    end
+    accordionDefaultButton2 = page.find_by_id("accordion-default-heading-2")
+    within accordionDefaultButton2 do
+      expect(accordionDefaultButton2[:class]).to eq "govuk-accordion__section-button"
     end
   end
 end
