f6cbb45e64270e75dbc57e03097919b672f945d5
jY Bee <jybetton7@outlook.com>
2021-10-19 16:12:04 +0100

    Starting with additions to spec

--- spec/features/accordion_spec.rb
+++ spec/features/accordion_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
-describe 'Accordion', :type => :feature do
-  it "should have correct number of steps" do
+describe 'Accordion components', :type => :feature do
+  before do
     visit '/accordion.html'
+  end
+
+  it "should have correct number of steps" do
     steps = page.find_by_id("26o7zVgA8zuq4GnxJgvjD8")
     within steps do
       has_css? 'div', count: 3
+    end
+  end
+
+  it "should have the right classes" do
+    defaultContentElement = page.find_by_id("accordion-default-content-0")
+    within defaultContentElement do
+      expect(defaultContentElement[:class]).to eq "govuk-accordion__section-content"
     end
   end
 end
