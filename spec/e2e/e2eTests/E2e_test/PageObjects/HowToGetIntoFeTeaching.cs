5ba6cbb0206d79869c7ebdce98ace9ceffc67670
Adam Clarkson <adamclarkson@Adams-MacBook-Pro.local>
2022-02-16 14:35:17 +0000

    updated e2e tests following content update

---
+++ spec/e2e/e2eTests/E2e_test/PageObjects/HowToGetIntoFeTeaching.cs
@@ -0,0 +1,92 @@
+﻿using OpenQA.Selenium;
+
+
+namespace TeachFe.E2e_test.PageObjects
+{
+    public class HowToGetIntoFeTeachingPage
+    {
+        private readonly IWebDriver driver;
+        public HowToGetIntoFeTeachingPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        #region Elements
+
+        public IWebElement MainContent
+        {
+            get { return driver.FindElement(By.Id("main-content")); }
+        }
+
+        public IWebElement ApplyForFEJobLink
+        {
+            get { return driver.FindElement(By.LinkText("How to apply for an FE teaching job")); }
+        }
+
+        public IWebElement ApprenticeshipLink
+        {
+            get { return driver.FindElement(By.LinkText("apprenticeship")); }
+        }
+
+        public IWebElement WhatLevelsMeanLink
+        {
+            get { return driver.FindElement(By.LinkText("What levels mean")); }
+        }
+
+        public IWebElement FETeachingApprenticeshipLink
+        {
+            get { return driver.FindElement(By.LinkText("FE teaching apprenticeship")); }
+        }
+
+        public IWebElement TrainInYourOwnTimeLink
+        {
+            get { return driver.FindElement(By.PartialLinkText("train in your own time")); }
+        }
+
+        public IWebElement EmailLink
+        {
+            get { return driver.FindElement(By.LinkText("Teach.FE@education.gov.uk")); }
+        }
+
+        #endregion
+
+        #region Actions
+
+        public bool ContentHowToGetIntoFeTeachingPageDisplayed()
+        {
+            return MainContent.Displayed;
+        }
+
+        public void ClickApplyForFEJobLink()
+        {
+            ApplyForFEJobLink.Click();
+        }
+
+        public void ClickApprenticeshipLink()
+        {
+            ApprenticeshipLink.Click();
+        }
+
+        public void ClickWhatLevelsMeanLink()
+        {
+            WhatLevelsMeanLink.Click();
+        }
+
+        public void ClickFETeachingApprenticeshipLink()
+        {
+            FETeachingApprenticeshipLink.Click();
+        }
+
+        public void ClickTrainInYourOwnTimeLink()
+        {
+            TrainInYourOwnTimeLink.Click();
+        }
+
+        public bool EmailLinkDisplayed()
+        {
+            return EmailLink.Displayed;
+        }
+
+        #endregion
+    }
+}
